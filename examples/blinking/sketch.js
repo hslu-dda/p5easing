d397426438b1e9fe806cf354f195d60b04b30425
miflck <contact@michaelflueckiger.ch>
2026-03-15 22:07:49 +0100

    fixing blinking example

--- examples/blinking/sketch.js
+++ examples/blinking/sketch.js
@@ -1,13 +1,21 @@
+let starttime = 0;
+let duration = 1000;
+let from = 0;
+let to = 255;
+
 function setup() {
   createCanvas(400, 400);
+  console.log(getEasingFunctionNames());
 }
 
 function draw() {
   background(220);
 
-  // Calculate alpha using easing - cycles every 1000ms (1 second)
-  let alpha = ease(millis() % 1000, 500, 0, 255, "easeInOutQuad");
-
+  let alpha = ease(starttime, duration, from, to, "easeInOutQuintic");
+  if (millis() > starttime + duration) {
+    starttime = millis();
+    [from, to] = [to, from];
+  }
   // Draw blinking rectangle
   fill(255, 0, 0, alpha);
   rect(150, 150, 100, 100);
