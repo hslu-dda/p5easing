efdaa51db33e65105f223ac943b858925c1d941e
miflck <contact@michaelflueckiger.ch>
2026-03-26 16:18:40 +0100

    new example

--- examples/Easing Steps/sketch.js
+++ examples/Easing Steps/sketch.js
@@ -5,9 +5,9 @@ function setup() {
 function draw() {
   background(220);
   for (let i = 0; i < 30; i++) {
-    let xPos = map(i, 0, 30, 10, width - 10 - 10);
-    let v = easeBySteps(i, 0, 30, 10, 200, "easeInQuintic");
-
+    let xPos = map(i, 0, 30, 10, width - 20);
+    let t = i / 30; // 0 → 1
+    let v = ease(10, 150, t, "easeInOutQuintic");
     rect(xPos, width / 2, 10, v);
   }
 }
